3007ea1d7d1f59478bcc16170eb6806d789a68b9
belek0 <lipendina.tn@gmail.com>
2020-04-27 17:55:49 +0300

    Исправлен assert

--- test_items.py
+++ test_items.py
@@ -1,10 +1,15 @@
 import time
+from selenium.common.exceptions import NoSuchElementException
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_add_to_bag(browser):
+def test_add_to_bag_button_exists(browser):
     browser.get(link)
     time.sleep(30)
-    btn_add_to_bag = browser.find_element_by_class_name("btn-add-to-basket")
-    assert btn_add_to_bag, 'Элемент не найден'
+    try:
+        btn_add_to_bag = browser.find_element_by_class_name("btn-add-to-basket")
+        result = True
+    except NoSuchElementException:
+        result = False
+    assert result is True, "Button is not found!"
